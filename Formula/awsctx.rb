17fae7f9afd7cefbf06ad051cceee94236008c81
Hironori Yamamoto <mr.nikoru918@gmail.com>
2022-03-05 13:34:51 +0900

    awsctx 0.3.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/awsctx.rb
+++ Formula/awsctx.rb
@@ -3,10 +3,10 @@
 class Awsctx < Formula
     desc "Context Manager for AWS Profiles"
     homepage "https://github.com/hiro-o918/awsctx"
-    version "0.2.0"
+    version "0.3.0"
     license "MIT"
-    url "https://github.com/hiro-o918/awsctx/archive/v0.2.0.tar.gz"
-    sha256 'old'
+    url "https://github.com/hiro-o918/awsctx/releases/download/v0.3.0/awsctx_v0.3.0_x86_64-apple-darwin.tar.gz"
+    sha256 '5553efe340c8ee8cf9d6f387ffa5881534e5445c0e6f23183ec17f20a1a02c72'
 
     def install
         bin.install "awsctx"
