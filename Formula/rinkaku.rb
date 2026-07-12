0eab944ef4981e1f7c14511e9971bd0ca7d99161
Hironori Yamamoto <mr.nikoru918@gmail.com>
2026-07-12 09:53:57 +0900

    fix: point rinkaku formula at v0.1.0 release assets

--- Formula/rinkaku.rb
+++ Formula/rinkaku.rb
@@ -6,7 +6,7 @@ class Rinkaku < Formula
     version "0.1.0"
     license "MIT"
     url "https://github.com/hiro-o918/rinkaku/releases/download/v0.1.0/rinkaku-x86_64-apple-darwin.tar.gz"
-    sha256 '0000000000000000000000000000000000000000000000000000000000000000'
+    sha256 'ad2866df4374a33d9f7aa54b73afd1b8a3e8dcedaae2da32bbbd6901d73868e6'
 
     def install
         bin.install "rinkaku"
